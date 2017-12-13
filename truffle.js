7a3ca4c96a39fc55022570d9f72ccb03089b231a
nginnever <ginneversource@gmail.com>
2017-12-13 12:35:21 -0800

    finish init

--- truffle.js
+++ truffle.js
@@ -1,9 +1,23 @@
+require('babel-register')
+require('babel-polyfill')
+
+var test = true
+var account
+
+if(test){
+  account = "0x01da6f5f5c89f3a83cc6bebb0eafc1f1e1c4a303"
+} else {
+  account = "0x5d0df28514c493ee11cf23f1b44f42516a7cb415"
+}
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      from: account,
+      gas: 4612388
     }
   }
 };
